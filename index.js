2ad79174c559073ea7c2603b0c96547ba602f6c4
Kandice CodeKid <you@example.com>
2019-08-09 16:25:48 -0500

    Completd all except ERROR challenge

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 //alert("Kandice")
 alert("Welcome");
 var Option = prompt("Press 1 to count   Press 2 to calculate    press 'q' to Quit");
+
+// if(Option === "q")
+
+
 if (Option ==="1")
+	{alert("Counting");
+	var OnesOrTwos = prompt("Press '1' to count by 1s or '2' for by 2s");
 {
-alert("counting by 1s");
-	var CountOne = parseInt(prompt("Enter number to count to"));
-
-}
+	if (OnesOrTwos==="1") 
+	{
+var ConOne = parseInt(prompt("Enter 1st Value"));
+var ConTwo = parseInt(prompt("Enter 2nd Value"));
+for (var x = ConOne; x <= ConTwo; x = ConOne++) 
 {
-CountInt = 
+	console.log(x)
 }
-console.log(CountOne++);
+}
+	else if (OnesOrTwos==="2") 
+	{
+var ConOneA = parseInt(prompt("Enter number to start"));
+var ConOneB = parseInt(prompt("Enter number to stop "));
+for (var x = ConOneA; x <= ConOneB; x = x+2) 
+{
+	console.log(x)
+}
+}
+}
+}
 
-
-
-if (Option==="2") 
+if (Option === "2") 
 {
 	alert("Calculating");
 	var CalOpt = prompt("Press 'a' for ADD and 's' for SUBTRACT");
@@ -35,11 +51,5 @@ var TotalVal2 = (ValOnea-ValTwoa);
 alert(TotalVal2);
 console.log(TotalVal2)
 }
-
-
-
 }
-//if (true) {}
 }
-//var CountOne =(prompt("Enter 1st number to count to")
-	
