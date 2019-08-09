1fa435a0f2ccb1e1e5a59f19d19287c749a20927
Kandice CodeKid <you@example.com>
2019-08-09 15:02:43 -0500

    2nd Push

--- index.js
+++ index.js
@@ -4,22 +4,42 @@ var Option = prompt("Press 1 to count   Press 2 to calculate    press 'q' to Qui
 if (Option ==="1")
 {
 alert("counting by 1s");
-	var CountOne = parseInt((prompt("Enter number to count to"));
+	var CountOne = parseInt(prompt("Enter number to count to"));
 
 }
 {
 CountInt = 
 }
 console.log(CountOne++);
+
+
+
 if (Option==="2") 
 {
-alert("Calculating");
-var ValOne = prompt("Enter Value");
-var ValTwo = prompt("Enter 2nd Value")
-var TotalVal = parseInt(ValOne + ValTwo)
-alert(TotalVal)
+	alert("Calculating");
+	var CalOpt = prompt("Press 'a' for ADD and 's' for SUBTRACT");
+{
+	if (CalOpt==="a") 
+	{
+var ValOne = parseInt(prompt("Enter Value"));
+var ValTwo = parseInt(prompt("Enter 2nd Value"));
+var TotalVal = (ValOne+ValTwo);
+alert(TotalVal);
 console.log(TotalVal)
-// if (true) {}
+	}
+if (CalOpt=== "s") 
+{
+var ValOnea = parseInt(prompt("Enter Value"));
+var ValTwoa = parseInt(prompt("Enter 2nd Value"));
+var TotalVal2 = (ValOnea-ValTwoa);
+alert(TotalVal2);
+console.log(TotalVal2)
+}
+
+
+
+}
+//if (true) {}
 }
 //var CountOne =(prompt("Enter 1st number to count to")
 	
