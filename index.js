4b52927dc257de9d78c91e104486751072cd5be9
Kandice CodeKid <you@example.com>
2019-08-09 14:33:38 -0500

    1st Push

---
+++ index.js
@@ -0,0 +1,25 @@
+//alert("Kandice")
+alert("Welcome");
+var Option = prompt("Press 1 to count   Press 2 to calculate    press 'q' to Quit");
+if (Option ==="1")
+{
+alert("counting by 1s");
+	var CountOne = parseInt((prompt("Enter number to count to"));
+
+}
+{
+CountInt = 
+}
+console.log(CountOne++);
+if (Option==="2") 
+{
+alert("Calculating");
+var ValOne = prompt("Enter Value");
+var ValTwo = prompt("Enter 2nd Value")
+var TotalVal = parseInt(ValOne + ValTwo)
+alert(TotalVal)
+console.log(TotalVal)
+// if (true) {}
+}
+//var CountOne =(prompt("Enter 1st number to count to")
+	
